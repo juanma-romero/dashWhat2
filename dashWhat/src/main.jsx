b0898f9bb5bf57941cdb2509dded3941b7612784
Juan Manuel Romero <xjuanma.romerox@gmail.com>
2025-01-14 09:30:05 -0300

    ListOrders hardcodeado

--- dashWhat/src/main.jsx
+++ dashWhat/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router"
 import './index.css'
 import App from './App.jsx'
+import ListOrders from './ListOrders.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" index element={<App />} />
+      <Route path="listado" element={<ListOrders />} />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
+
+
